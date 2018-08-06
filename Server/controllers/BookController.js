3eecf3b4c0b0adeacab9e3149e8bcf3c04d3810e
chinonso williams <williamschinonso11@gmail.com>
2018-08-06 12:41:28 +0100

    created a bookController

---
+++ Server/controllers/BookController.js
@@ -0,0 +1,35 @@
+var model = require ('../models./Book');
+
+exports.addBook = function (req, res){
+    var data = {
+        author: req.body.author,
+        title: req.body.title,
+        price: req.body.price,
+        bookurl: req.body.bookurl,
+        comments: req.body.comments,
+        commentCount: req.body.commentCount,
+        buyerCount: req.body.buyerCount,
+        ikenga: req.body.ikenga
+    };
+}
+
+exports.deleteBook = function (req, res){
+    var data = {_id: req.params.id};
+}
+
+exports.updateBook = function (req, res){
+    var id = req.params.id;
+    var options = req.body;
+}
+
+exports.getBookById = function (req, res){
+    var id = req.params.id;
+}
+
+exports.getAllBooks = function (req, res){
+
+}
+
+exports.getBookByParam = function (req, res){
+     var options = req.query;
+}
