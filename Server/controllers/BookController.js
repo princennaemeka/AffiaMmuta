26e5ecd6b88b92072726905679d139a5057b6112
chinonso williams <williamschinonso11@gmail.com>
2018-08-06 15:40:58 +0100

    called the service function to all methods

--- Server/controllers/BookController.js
+++ Server/controllers/BookController.js
@@ -1,4 +1,5 @@
 var model = require ('../models/Book');
+var model = require ('../services/BookService');
 
 exports.addBook = function (req, res){
     var data = {
@@ -11,25 +12,30 @@ exports.addBook = function (req, res){
         buyerCount: req.body.buyerCount,
         ikenga: req.body.ikenga
     };
+    return service.addBook(req, res, data);
 }
 
 exports.deleteBook = function (req, res){
     var data = {_id: req.params.id};
+    return service.deleteBook(req, res, deleteBook);
 }
 
 exports.updateBook = function (req, res){
     var id = req.params.id;
     var options = req.body;
+    return service.updateBook(req, res, id, options);
 }
 
 exports.getBookById = function (req, res){
     var id = req.params.id;
+    return service.getBookById(req, res, id);
 }
 
 exports.getAllBooks = function (req, res){
-
+return service.getAllBooks(req, res);
 }
 
 exports.getBookByParam = function (req, res){
      var options = req.query;
+     return service.getBookByParam(req, res, options);
 }
