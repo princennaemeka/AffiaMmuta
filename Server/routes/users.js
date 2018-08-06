a76b64b838bb0b5fa0801c8b6720aa1df8bd6443
Ibesoft11 <ibesoft11@gmail.com>
2018-08-06 12:43:27 +0100

    Added required routes for accessing the User controller

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/UserController');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/create', userController.addUser);
+router.get('/', userController.getUsers);
+router.get('/delete/:id', userController.deleteUser);
+router.get('/search', userController.getUserByParam);
+router.post('/update/:id', userController.updateUser);
+
 
 module.exports = router;
