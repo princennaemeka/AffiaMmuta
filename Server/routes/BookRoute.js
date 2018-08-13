464e49a59f684bfd3198b5da1f57b4eaa20f2011
ibesoft11 <ibesoft11@gmail.com>
2018-08-13 10:51:23 +0100

    Synchronized code with remote version

--- Server/routes/BookRoute.js
+++ Server/routes/BookRoute.js
@@ -4,20 +4,12 @@ var model = require('../models/Book');
 var BookController = require ('../controllers/BookController');
 var uploadService = require('../services/uploadService');
 
-
-
-
 /* GET BookRoute listing. */
 router.get('/', BookController.getBooks);
-
 router.get('/delete/:id', BookController.deleteBook);
-
 router.get('/search', BookController.getBookByParam);
-
 router.get('/book/:id', BookController.getBookById);
-
 router.post('/update/:id', BookController.updateBook);
-
 router.post('/addBook', uploadService.upload.any(), BookController.addBook);
 
 module.exports = router;
