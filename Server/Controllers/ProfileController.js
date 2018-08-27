325eccdfea442044a1b56b76eb97987277e05bd4
ibesoft11 <ibesoft11@gmail.com>
2018-08-27 13:39:18 +0100

    Error handling for service calls

--- Server/Controllers/ProfileController.js
+++ Server/Controllers/ProfileController.js
@@ -1,8 +1,6 @@
 var model = require('../Models/User');
 var libraryService = require('../Services/LibraryService');
 var bookService = require('../services/BookService');
-const Joi = require('joi');
-const bcrypt = require('bcrypt');
 
 //this should run after successful book(s) purchase
 exports.addBookToLibrary = function(req, res){
@@ -10,23 +8,35 @@ exports.addBookToLibrary = function(req, res){
         user: req.body.user,
         books: req.body.books
     };
-    return libraryService.addBookToLibrary(req, res, data);
-    //make call to library service
+    try {
+        return libraryService.addBookToLibrary(req, res, data);
+    } catch (exception){
+        console.log("Error : "+exception);
+    }
 }
 
 exports.readBook = function(req, res){
-    var book = req.query.book;
-    return bookService.getBookById(req, res, book);
-    //get book from book service
+    try {
+        var book = req.query.book;
+        return bookService.getBookById(req, res, book);
+    } catch (exception){
+        console.log("Error : "+exception);
+    }
 }
 
 exports.viewLibrary = function(req, res){
-    var user = req.query.user;
-    return libraryService.getBooksFromLibrary(req, res, {user: user});
-    //get library from library service
+    try {
+        var user = req.query.user;
+        return libraryService.getBooksFromLibrary(req, res, {user: user});
+    } catch(exception){
+        console.log("Error : "+exception);
+    }
 }
 
 exports.clapForABook = function(req, res){
-    return bookService.increaseClap(req, res, req.query.book, Number.parseInt(req.query.ikenga));
-    //make call to book service
+    try {
+        return bookService.increaseClap(req, res, req.query.book, Number.parseInt(req.query.ikenga));
+    } catch (exception){
+        console.log("Error : "+exception);
+    }
 }
