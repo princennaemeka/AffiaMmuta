6b50188dad40674f0061c25d4a1aaba7b0a201c1
chinonso williams <williamschinonso11@gmail.com>
2018-08-28 08:23:55 +0100

    created delete function

--- Server/Services/CategoryService.js
+++ Server/Services/CategoryService.js
@@ -7,3 +7,10 @@ exports.addCategory = function (req, res, data){
         res.json ({message: 'category added successfully'});
     });
 }
+
+exports.deleteCategory = function (req, res, id){
+    repository.getById(id, function(err){
+        if (err) res.json ({err: err, message: 'errror, category could not be deleted'});
+        res.json ({message: 'category deleted successfully'});
+    });
+}
