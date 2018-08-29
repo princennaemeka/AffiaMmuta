b43b257cfd2e773eb3a0ed1af18640ac186dc28f
chinonso williams <williamschinonso11@gmail.com>
2018-08-29 14:07:47 +0100

    edited addCategory function

--- Server/Controllers/CategoryController.js
+++ Server/Controllers/CategoryController.js
@@ -3,7 +3,6 @@ var service = require('../Services/CategoryService');
 
 exports.addCategory = function (req, res){
     var data = {
-        book: req.body.book,
         categoryTitle: req.body.categoryTitle
     };
     return service.addCategory(req, res, data);
@@ -21,4 +20,9 @@ exports.getCategoryById = function (req, res){
 
 exports.getCategory = function (req, res){
     return service.getAllCategories(req, res);
+}
+
+exports.getCategoryByParam = function (req, res){
+     var options = req.query;
+     return service.getCategoryByParam(req, res, options);
 }
