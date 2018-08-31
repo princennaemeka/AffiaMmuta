0b7f10528a1e84c3abe58d4caef3734221290145
chinonso williams <williamschinonso11@gmail.com>
2018-08-31 15:39:02 +0100

    added route

--- Server/routes/BookRoute.js
+++ Server/routes/BookRoute.js
@@ -14,11 +14,7 @@ router.post('/update/:id', BookController.updateBook);
 router.post('/addBook', uploadService.upload.any(), BookController.addBook);
 router.get('/latest', BookController.getLatestBooks);
 router.get('/:category', BookController.getBooksByCategory);
-<<<<<<< HEAD
-router.get('/search/:title',BookController.searchBooks);
-=======
 router.get('/book/:id/comments', CommentController.getComments);
 router.post('/book/:id/create', CommentController.addComment);
->>>>>>> 21757cfdd5dcfcf4677301a53dac7676ce30556a
 
 module.exports = router;
